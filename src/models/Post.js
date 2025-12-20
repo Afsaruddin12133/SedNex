0917e015a9c066eac6c1a289aac78314b0a98731
Afsaruddin12133 <afsaruddin12133@gmail.com>
2025-12-20 13:29:11 +0600

    Post Management update

---
+++ src/models/Post.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 2000,
+    },
+    lovedBy: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    loveCount: {
+      type: Number,
+      default: 0,
+    },
+
+    commentsCount: {
+      type: Number,
+      default: 0,
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Post", postSchema);
---
+++ src/routes/post.routes.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const authMiddleware = require("../middlewares/auth.middleware");
+const { createPost, getPosts, toggleLove, getPostById, deletePost } = require("../controllers/post.controller");
+const { createComment, getPostComments, getReplies } = require("../controllers/comment.controller");
+const adminMiddleware = require("../middlewares/admin.middleware");
+
+const router = express.Router();
+// ========================
+// Create Posts 
+// ========================
+
+router.post(
+    "/", 
+    authMiddleware, 
+    createPost
+);
+
+
+// ========================
+// Get Posts with Pagination
+// ========================
+router.get(
+    "/", 
+    authMiddleware, 
+    getPosts
+);
+
+// ========================
+// Get Posts By Id
+// ========================
+router.get(
+    "/:postId",
+    authMiddleware,
+    getPostById
+);
+
+
+router.delete(
+    "/:postId", 
+     authMiddleware, 
+    adminMiddleware,
+    deletePost
+);
+
+// ========================
+// Posts Love and Unlove
+// ========================
+
+router.patch(
+    "/:postId/love", 
+    authMiddleware, 
+    toggleLove
+);
+
+// ========================
+// Posts comment
+// ========================
+
+router.post(
+    "/comment/:postId", 
+    authMiddleware, 
+    createComment
+);
+
+router.get(
+    "/comment/:postId", 
+    authMiddleware,
+    getPostComments
+);
+router.get(
+    "/comment/replies/:commentId",
+    authMiddleware,
+    getReplies
+);
+
+module.exports = router;
